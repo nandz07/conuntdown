9e61365e0771fad34fba535622c52c977d983c77
Nandakumar T <nandakumart07@gmail.com>
2024-01-02 21:18:05 +0530

    countdown complete

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,33 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Countdown.css'
 
 function Countdown() {
+    const [days, setDays] = useState(0)
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+    const [inputDate, setInputDate] = useState('01 june 2024')
+    const [currentDate, setCurrentDate] = useState(inputDate)
+   
+
+    useEffect(() => {
+        const changingDate = new Date(inputDate)
+        const currentDate = new Date()
+        const totalSeconds = (changingDate - currentDate) / 1000
+
+        setDays(formatTime(Math.floor(totalSeconds / 3600 / 24)))
+        setHours(Math.floor(totalSeconds / 3600) % 24)
+        setMinutes(Math.floor(totalSeconds / 60) % 60)
+        setSeconds(Math.floor(totalSeconds % 60))
+        setTimeout(() => {
+            setSeconds(seconds-1)
+        }, 1000);
+    },[currentDate,seconds])
+    function formatTime(time) {
+        return time < 10 ? `0${time}` : time
+    }
+
+    const onChangeHandler=(event)=>{
+        setInputDate(event.target.value)
+    }
+    const onclickhandler=()=>{
+        setCurrentDate(inputDate)
+    }
     return (
         <div className='countdown-container'>
             <div className="countdown-values">
                 <div className="countdown-value">
-                    <p className="big-text">
-                        days
-                    </p>
+                    <p className="big-text">{days}</p>
+                    <span>days</span>
                 </div>
-                <span>days</span>
                 <div className="countdown-value">
                     <p className="big-text">
-                        hours
+                        {hours}
                     </p>
+                    <span>hours</span>
                 </div>
-                <span>hours</span>
                 <div className="countdown-value">
                     <p className="big-text">
-                        mins
+                        {minutes}
                     </p>
+                    <span>minutes</span>
                 </div>
-                <span>mins</span>
                 <div className="countdown-value">
                     <p className="big-text">
-
+                        {seconds}
                     </p>
+                    <span>seconds</span>
                 </div>
-                <span>days</span>
+            </div>
+            <div className="countdown-input-button">
+                <input type="date" className="countdown-input" onChange={onChangeHandler}/>
+                <button className="countdown-button" onClick={onclickhandler}  >
+                    countdown
+                </button>
             </div>
         </div>
     )
